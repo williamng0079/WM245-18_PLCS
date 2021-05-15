72bf4a3f36cb66f2f7c3ee38a0b50ac3cdcf6fdc
williamng0079 <williamng0079@gmail.com>
2021-05-16 00:15:46 +0100

    added b64 encoding to the encrypted binary and allowed file IO

--- RSA_encrypt.c
+++ RSA_encrypt.c
@@ -6,26 +6,42 @@
 #include <openssl/evp.h>
 #include <openssl/bio.h>
 #include <openssl/err.h>
+#include "base64.h"  
 
+/* Header file "base64.h" is used to convert the encryptped binary into human readable text so it can be encoded later into stegano objects easiler */
+/* Base64.h header file source author: Sherif, W. (2013). */
 /* https://www.openssl.org/docs/man1.1.1/man3/ : The documentation for the openssl version 1.1.1 c library function calls, all fucntion explainations below are researched from this source. */
 /* http://hayageek.com/rsa-encryption-decryption-openssl-c/ : The source reference of this code, author: Kusuma, R (2014) */
 /* Prior to running the program, please install openssl dev package using sudo apt-get install libssl-dev */
-
 /* The code can be compiled with "gcc -o RSA_encrypt RSA_encrypt.c -lcrypto  " */
 
 
 /* Function prototypes */
 RSA * makeRSA(char * filename, int keytype);
 int public_encryption(unsigned char * msg, int msg_len, char * filename, unsigned char * encrypted_msg);
+void write_b64file(char * b64_msg);
+
+
 
 
 /* Program Entry */
 int main()
 {
-    char msg[2048/8] = "This is the secret message to be encrypted";    // Storing message withe 2048/8 limit number of chars
+    char msg[2048/8] = {};                                              // Storing message withe 2048/8 limit number of chars
     char * pubkey = "stegano-example-public.pem";                       // Calling the key file name
     unsigned char encrypted_msg[4098] = {};                             // Creating a variable later to be used to store the encrypted binary
     int encrypted_length;                                               // Variable to store the len of the binary
+    int encoded_len;                                                    // Store length of the encoded msg
+    char * encoded_msg;                                                 // Variable for the encoded message
+
+    
+    char * secret_file = "secret_message.txt";                          // This section will be able to read a user supplied text file and extract secret message within
+    FILE * fsecret = fopen(secret_file, "rb");
+    fgets(msg, 256, fsecret);
+    fclose(fsecret);
+    printf("%s\n",msg);
+    
+    
 
     encrypted_length = public_encryption(msg, strlen(msg), pubkey, encrypted_msg); // Calling the encryption function
 
@@ -35,18 +51,26 @@ int main()
         exit(0);
     }
     
-    //printf("The encryped message is %s\n", encrypted_msg);             // Display encrypted message for testing 
+    encoded_msg = base64(encrypted_msg, encrypted_length, &encoded_len);// This function calls the base64.h header file and is able to process the binary data into human-readable base64 text.
 
-    printf("The size of the encrypted message is %d\n", encrypted_length); 
-    
-    FILE * f2 = fopen("RSA_encrypted_msg.bin", "wb");                    // Create a file to store and observe the encrypted binary output
+    printf("The encoded message is:  %s\n", encoded_msg);
+    printf("The encoded length is:  %d\n", encoded_len);
 
-    fputs(encrypted_msg, f2);
-    fclose(f2);
-    
+    write_b64file(encoded_msg);
+
+    //printf("The encryped message is %s\n", encrypted_msg);             // Display encrypted message info for testing 
+    //printf("The size of the encrypted message is %d\n", encrypted_length); 
+
+    //FILE * f2 = fopen("RSA_encrypted_msg.bin", "wb");                  // Create a file to store and observe the encrypted binary output, used to test output of the encryption
+    //fputs(encrypted_msg, f2);
+    //fclose(f2);
     return 0;
     
 }
+
+
+
+
 
 // The following function will have the ability to read the public key file stored within the same directory and create a RSA structure 
 RSA * makeRSA(char * filename, int keytype)                             // declaring a function with the pointer return type 
@@ -73,8 +97,9 @@ RSA * makeRSA(char * filename, int keytype)                             // decla
 } 
 
 
-// The next function is used to encrypt message with the rsa structure defined previously 
 
+
+// The next function is used to encrypt message with the rsa structure defined previously 
 int public_encryption(unsigned char * msg, int msg_len, char * filename, unsigned char * encrypted_msg)
 {
     int padding = RSA_PKCS1_OAEP_PADDING;           // Invoke padding to introduce randomness, OAEP is choose 
@@ -83,8 +108,19 @@ int public_encryption(unsigned char * msg, int msg_len, char * filename, unsigne
     
     RSA *rsa = makeRSA(filename, 1);
     
-    output_size = RSA_public_encrypt(msg_len, msg, encrypted_msg, rsa, padding);    // Encryption process
+    output_size = RSA_public_encrypt(msg_len, msg, encrypted_msg, rsa, padding);    // Encryption function process
 
     return output_size;
 } 
 
+
+
+
+// This funtion will be able to write the encoded message into a text file
+void write_b64file(char * b64_msg)
+{
+    char * b64file = "b64_encoded_output.txt";
+    FILE * f64 = fopen(b64file, "wb");
+    fputs(b64_msg, f64);
+    fclose(f64);
+}
